cae9383a211fa9916527f1998ce5d2b26e0b643c
mtsluna <mtsluna@gmail.com>
2020-08-31 19:51:05 -0300

    .NET Core to .NET Framework

---
+++ Lab2Ej192/Program.cs
@@ -0,0 +1,17 @@
+﻿using EjercicioPag192.ejercicio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Ej192
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio.exec();
+        }
+    }
+}
