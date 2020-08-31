bf7ed5daa0cc93a78c73b691e904278229801a11
Gianfranco Curci <gianuniversidad@gmail.com>
2020-08-30 22:11:53 -0300

    Primer Commit

---
+++ EjercicioPag192/Program.cs
@@ -0,0 +1,12 @@
+﻿using EjercicioPag192.ejercicio;
+using System;
+
+namespace EjercicioPag192
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ EjercicioPag192/ejercicio/Ejercicio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioPag192.ejercicio
+{
+    class Ejercicio
+    {
+
+        public static int pedirCantidadAlumnos()
+        {
+            Console.Write("Ingrese la cantidad de alumnos: ");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        public static void pedirCalificaciones(int [] calificaciones, int elem)
+        {
+            Console.Write("Ingrese la nota del "+(elem+1)+"° alumno: ");
+            int calificacion = Int32.Parse(Console.ReadLine());
+            calificaciones[elem] = calificacion;
+        }
+
+        public static void mostrarResumen(int[] cotas, double promedio)
+        {
+            Console.WriteLine("Nota menor: " + cotas[0]);
+            Console.WriteLine("Nota mayor: " + cotas[1]);
+            Console.WriteLine("Promedio: " + Math.Round(promedio, 2));
+        }
+
+    }
+}
